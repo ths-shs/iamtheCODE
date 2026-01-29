31c91ef1c22514bd940d0c47031ef1141c8d1374
neil30 <neil.tamao30@gmail.com>
2026-01-29 16:55:06 +0800

    floating button

--- javascript/script.js
+++ javascript/script.js
@@ -42,3 +42,20 @@ faqs.forEach((faq) => {
         faq.classList.toggle("active");
     });
 });
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        scrollTopBtn.classList.add("show");
+    } else {
+        scrollTopBtn.classList.remove("show");
+    }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
